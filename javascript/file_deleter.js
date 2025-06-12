b4b0f2229116886c6a54c2311694f77eb80d0a37
nabilaba <nabilazizbimaanggita@gmail.com>
2025-06-12 13:23:19 +0700

    add new feature and some change

--- javascript/file_deleter.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click", (e) => {
-    if (e.target.innerText === "Delete") {
-        setTimeout(() => {
-            document.querySelector('[aria-label="Folder"]').dispatchEvent(new Event('change'))
-        }, 500);
-    }
-});
